4c8af3f75d3dd19502948c447751c0d8db93b195
Aadhith <aadhith@asherdn.com>
2026-05-26 21:52:32 +0530

    auth-middleware

--- app.js
+++ app.js
@@ -3,4 +3,9 @@ const app = express();
 const dotenv = require("dotenv").config();
 
 const port = process.env.PORT;
+
 app.use(express.json());
+
+app.listen(port,()=>{
+    console.log(`server running ${port}`);
+});
