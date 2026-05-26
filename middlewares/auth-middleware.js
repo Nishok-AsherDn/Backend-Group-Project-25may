18df406f75360d54d236d856ecf9a5f30fa31de0
Aadhith <aadhith@asherdn.com>
2026-05-26 14:44:44 +0530

    add middleware - auth-middleware

---
+++ middlewares/auth-middleware.js
@@ -0,0 +1,38 @@
+const asyncHandle = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+
+const validateToken = asyncHandle(async(req,res,next)=>{
+    let token;
+        let authHeader = req.headers.authorization || req.headers.Authorization;
+
+    if (authHeader && authHeader.startsWith("Bearer")) {
+
+        token = authHeader.split(" ")[1];
+
+        jwt.verify(
+            token,process.env.ACCESS_TOKEN_SECRET,(err, decoded) => {
+
+                if (err) {
+                    res.status(401);
+                    throw new Error("Token invalid");
+                }
+
+                console.log(decoded.user);
+
+                req.user = decoded.user;
+
+                next();
+
+            }
+        );
+
+    } else {
+
+        res.status(401);
+        throw new Error("Token missing");
+
+    }
+
+});
+
+module.exports = validateToken;
