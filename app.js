65e82408872e6a005d6371efaa408825cda0c662
Nishok <nishok@asherdn.com>
2026-05-27 17:09:11 +0530

    Removed manual port number

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv").config();
 const taskRoutes = require("./routes/task-routes");
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use("/tasks", taskRoutes);
